9ae4e2b5df279efe8cbe1117233f02b176e08e9e
Andreas Skaret <andreas.skaret@gmail.com>
2021-09-26 23:02:33 +0200

    Added validation on Add Beer

--- BlazorTest/ViewModels/BeerViewModel.cs
+++ BlazorTest/ViewModels/BeerViewModel.cs
@@ -6,11 +6,20 @@ namespace BlazorTest.ViewModels
     public class BeerViewModel
     {
         public int BeerId { get; set; }
+        [Required(ErrorMessage = "Beer style is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Beer style is required.")]
         public int BeerStyleId { get; set; }
-        public int BreweryId { get; set; }
-        public int BeerClassId { get; set; }
 
+        [Required(ErrorMessage = "Brewery is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Brewery is required.")]
+        public int BreweryId { get; set; }
+
+        [Required(ErrorMessage = "Beer class is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Beer class is required.")]
+        public int BeerClassId { get; set; }
+                
         public BeerstyleModel? BeerStyleModel {  get; set; }
+        
         public BeerclassModel? BeerclassModel { get; set; }
 
         [Required]
@@ -19,6 +28,7 @@ namespace BlazorTest.ViewModels
         public string Name { get; set; }
 
         [Required]
+        [Range(0.0, 25.0, ErrorMessage = "ABV must be positive and less than 25")]
         public float ABV { get; set; }
         public string? RateBeerLink { get; set; }
     }
