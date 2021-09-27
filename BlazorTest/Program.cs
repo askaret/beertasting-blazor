5d95dcb8d02676fb250f74f5a302a3c702471380
Andreas Skaret <andreas.skaret@gmail.com>
2021-09-27 22:43:15 +0200

    Added authentication with Azure AD

--- BlazorTest/Program.cs
+++ BlazorTest/Program.cs
@@ -1,14 +1,37 @@
 using DataAccessLibrary;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddMudServices();
+builder.Services.AddHttpClient();
 builder.Services.AddTransient<ISqlDataAccess, SqlDataAccess>();
 builder.Services.AddTransient<IBeertastingRepository, BeertastingRepository>();
+
+
+if (!builder.Environment.IsDevelopment())
+{
+    builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAD"))
+                    // be sure to request all required permissions up-front
+                    .EnableTokenAcquisitionToCallDownstreamApi(new[] { "User.Read", "https://management.azure.com/user_impersonation" })
+                    .AddInMemoryTokenCaches();
+
+    builder.Services.AddControllersWithViews(options =>
+    {
+        var policy = new AuthorizationPolicyBuilder()
+            .RequireAuthenticatedUser()
+            .Build();
+        options.Filters.Add(new AuthorizeFilter(policy));
+    }).AddMicrosoftIdentityUI();
+}
 
 var app = builder.Build();
 
