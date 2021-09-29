b77d8db2186b44bdc9728c2c6509cc31fbaf0459
Andreas Skaret <andreas.skaret@gmail.com>
2021-09-29 11:36:05 +0200

    Hacky fix for connection string lookup when running in Azure

--- DataAccessLibrary/SqlDataAccess.cs
+++ DataAccessLibrary/SqlDataAccess.cs
@@ -15,15 +15,11 @@ namespace DataAccessLibrary
         {
             _configuration = configuration;
 
-            try
-            {
-                var connectionString = _configuration.GetConnectionString(ConnectionStringName);
-            }
-            catch (Exception)
-            {
+            var connectionString = _configuration.GetConnectionString(ConnectionStringName);
 
+            if (connectionString == null)
                 ConnectionStringName = "AZURE";
-            }
+
         }
 
         public async Task<List<T>> LoadData<T, U>(string query, U parameters)
@@ -48,7 +44,7 @@ namespace DataAccessLibrary
             var connectionString = _configuration.GetConnectionString(ConnectionStringName);
 
             using IDbConnection connection = new SqlConnection(connectionString);
-            return await connection.QueryFirstOrDefaultAsync<T>(query, parameters);            
+            return await connection.QueryFirstOrDefaultAsync<T>(query, parameters);
         }
 
         public async Task DeleteData<T>(string sql, T parameters)
