e390419c575ced2fb5fe39103ec96b03a8daa4dd
lowren <lowren92@gmail.com>
2021-10-02 01:21:45 +0200

    Added VoteModel

---
+++ DataAccessLibrary/Models/VoteModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Models
+{
+    public class VoteModel
+    {
+        public int VoteId {  get; set; }
+        public int BeerId { get; set; }
+        public int TastingId { get;set; }
+        public int TasterId {  get; set; }
+        public double Taste {  get; set; }
+        public double Appearance {  get; set; }
+        public double Overall {  get; set; }
+        public string Note {  get; set; }
+    }
+}
